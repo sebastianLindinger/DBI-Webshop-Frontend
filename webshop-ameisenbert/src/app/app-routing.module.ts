a0762d4a626bb0f3ba50a3e19aac49549491dbae
seiglmeg@gmail.com <seiglmeg@gmail.com>
2021-05-16 16:30:33 +0200

    created Componenents and implemented routing-logic

--- webshop-ameisenbert/src/app/app-routing.module.ts
+++ webshop-ameisenbert/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'products', component: ProductsComponent},
+  { path: 'orders', component: OrdersComponent},
+  { path: 'cart', component: CartComponent},
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- webshop-ameisenbert/src/app/app.module.ts
+++ webshop-ameisenbert/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductsComponent } from './products/products.component';
+import { OrdersComponent } from './orders/orders.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductsComponent,
+    OrdersComponent,
+    CartComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
