5acb6e990a3bdb5029c294d369b60b53a03f9a88
seiglmeg@gmail.com <seiglmeg@gmail.com>
2021-05-17 14:08:19 +0200

    add login component

--- webshop-ameisenbert/src/app/login/login.component.ts
+++ webshop-ameisenbert/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/servies/auth.service';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit() { }
+
+  onSubmit(f: NgForm) {
+    /*this.alertService.info('Checking User Info');
+    this.progressBar.startLoading();
+    const loginObserver = {
+      next: x => {
+        this.progressBar.setSuccess();
+        console.log('User logged in');
+        this.alertService.success('Logged In');
+        this.progressBar.completeLoading();
+      },
+      error: err => {
+        this.progressBar.setError();
+        console.log(err);
+        this.alertService.danger('Unable to Login');
+        this.progressBar.completeLoading();
+      }
+    };
+    this.authService.login(f.value).subscribe(loginObserver);
+*/
+    console.log('button Submit clicked')
+    this.authService.login(f.value);
   }
 
 }
