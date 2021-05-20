fdfe6c0694ab72430fb41fafc8185a562599ffa4
seiglmeg@gmail.com <seiglmeg@gmail.com>
2021-05-20 09:06:46 +0200

    add image

--- webshop-ameisenbert/src/app/product/product.ts
+++ webshop-ameisenbert/src/app/product/product.ts
@@ -2,5 +2,6 @@ export class product {
     price: number | undefined;
     weight: number | undefined;
     name: string | undefined;
+    image: string | undefined;
     productID: number | undefined;
 }
