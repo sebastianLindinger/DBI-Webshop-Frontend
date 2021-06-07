f4581b838be640ee8434bf9b36cb94a8bdd3157f
seiglmeg@gmail.com <seiglmeg@gmail.com>
2021-06-07 15:13:56 +0200

    add register logic

--- webshop-ameisenbert/src/app/register/register.component.ts
+++ webshop-ameisenbert/src/app/register/register.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, NgForm, Validators, FormBuilder } from '@angular/forms';
+import { HttpClientService } from '../services/http-client.service';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  constructor(private httpservice: HttpClientService) { }
 
-  ngOnInit(): void {
+  textFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+
+  username: string = '';
+  password: string = '';
+  email: string = '';
+
+  onSubmit() {
+    console.log('Register clicked')
+
+    var model = {
+      userName: this.username,
+      password: this.password,
+      email: this.email
+    }
+
+    this.httpservice.register(model).subscribe((data: any) => {
+      console.log(data);
+      if (data.hasOwnProperty('userID')) this.httpservice.createCartForUser(data['userID']).subscribe(data => console.log(data));
+    });
   }
-
 }
